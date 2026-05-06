165773a1014ac20cb55cf3a89763ba60929a87f3
iafuad <iafuad@proton.me>
2026-05-06 22:13:35 +0600

    Update Profile model with additional fields; register Profile in admin

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -32,10 +32,44 @@ class User(AbstractUser):
     objects = UserManager()
 
 
+# class AccountStatus(models.Model):
+#     name = models.CharField(max_length=50)
+
+#     def save(self, *args, **kwargs):
+#         self.name = self.name.capitalize()
+#         super().save(*args, **kwargs)
+
+#     def __str__(self):
+#         return self.name
+
+
 class Profile(models.Model):
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("NORMAL", "Normal"),
+        ("FLAGGED", "Flagged"),
+        ("SUSPENDED", "Suspended"),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
-    # add extra fields later
-    first_name = models.CharField(max_length=32, blank=False)
-    last_name = models.CharField(max_length=32, blank=True)
+    first_name = models.CharField(max_length=32, null=True, blank=True)
+    last_name = models.CharField(max_length=32, null=True, blank=True)
+    student_id = models.IntegerField(unique=True, null=True, blank=True)
     bio = models.TextField(blank=True)
+    profile_picture = models.OneToOneField(
+        "media.Photo",
+        on_delete=models.CASCADE,
+        related_name="profile_picture",
+        null=True,
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="PENDING",
+    )
+    is_verified = models.BooleanField(default=False)
+    is_graduated = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.user.email})"
