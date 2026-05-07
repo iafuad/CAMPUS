1ab9ed9bbed5bd40f9a3e389f4fdc418937c7472
Shahriar-Hossain-Choton <shahriarhc0708@gmail.com>
2026-05-08 02:33:38 +0600

    added ranking system

--- apps/rankings/models.py
+++ apps/rankings/models.py
@@ -1,3 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class UserRank(models.Model):
+    name = models.CharField(max_length=50)
+    max_xp = models.PositiveIntegerField(default=0)
+    min_xp = models.PositiveIntegerField(default=0)
