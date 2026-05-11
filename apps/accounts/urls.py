80ab62ba6815e277acf6f74fe03b039e9c95e6e5
iafuad <iafuad@proton.me>
2026-05-12 03:22:32 +0600

    Refactor URL namespacing in accounts app: update all URL references in templates and views to include the 'accounts' namespace for consistency and clarity.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import myself, profile_view, edit_profile, register
 
+app_name = "accounts"
+
 urlpatterns = [
     path(
         "login/",
